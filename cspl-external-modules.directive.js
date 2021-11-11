cc44580439fb1538218520ce93b3b0ebc55e03f3
Akshayaa11 <94050165+Akshayaa11@users.noreply.github.com>
2021-11-11 13:02:01 +0530

    Add files via upload

---
+++ cspl-external-modules.directive.js
@@ -0,0 +1,35 @@
+(function() {
+    'use strict';
+    angular
+        .module('nimbusApp')
+        .directive('csplExternalModules', ['$compile', 'ExternalModules',
+                                         function($compile, ExternalModules) {
+
+            function linkFunction(scope, iElement, iAttrs) {
+
+                var templateContent = '';
+
+                angular.forEach(scope.modules, function(module){
+                    if(module == ExternalModules.DocumentManagement){
+                        templateContent = templateContent + '<cspl-document-upload></cspl-document-upload>';
+                    }
+                    else if(module == 'Search'){
+                        templateContent = templateContent + '<cspl-search ng-model=\'input\'></cspl-search>'
+                    }
+                })
+
+                var template = $compile(templateContent)(scope);
+                iElement.after(template);
+
+            }
+
+            return {
+                restrict: 'E',
+                scope: {
+                    modules: '=',
+                },
+                link: linkFunction,
+                transclude: true
+            };
+        }]);
+})();
