251b6b4f4085933a763df16d671f52f79bba172e
Akshayaa11 <94050165+Akshayaa11@users.noreply.github.com>
2021-11-11 13:56:11 +0530

    Update cspl-external-modules.constant.js

--- cspl-external-modules.constant.js
+++ cspl-external-modules.constant.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
     angular
-        .module('csplExternalModulesModule')
+        .module('csplExternalModules')
         .constant('ExternalModules', {
         	
             DocumentManagement : 'DocumentManagement'
